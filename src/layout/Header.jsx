b1360a71fa0aad42ec600c32daf9b5cef4c01e02
Christopher <46559129+Christonn93@users.noreply.github.com>
2024-03-05 08:38:52 +0100

    Setup
    Starting the setup of the site.

---
+++ src/layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+
+const Header = () => {
+ return (
+  <>
+   <Box
+    sx={{
+     display: "flex",
+     justifyContent: "space-between",
+     p: 2,
+    }}
+   ></Box>
+  </>
+ );
+};
+
+export default Header;
